18be631c57f9c3d841e072116e6cea60665dc6c3
Koh-Du-Beom <beanleaf3260@gmail.com>
2024-03-13 12:58:30 +0900

    commit

---
+++ Lv1/lv1_12.js
@@ -0,0 +1,16 @@
+// 프로그래머스 lv1 둘만의 암호
+
+function solution(s, skip, index) {
+  s.forEach((item) => {
+    let indexedItem = item.charCodeAt(0);
+    for (let i=0; i<index; i++){
+      if (s === skip[i]){
+
+      }
+    }
+    // for문을 무사히 벗어났다면, 즉 skip안에 있는 문자열이 아니라면
+    
+  })
+  var answer = '';
+  return answer;
+}
---
+++ Lv1/programmers_lv1_06.js
@@ -0,0 +1,26 @@
+// 프로그래머스 lv 1 문자열 내 마음대로 정렬하기
+
+function solution(strings, n){
+
+  var answer = [];
+  strings.sort((str1, str2)=>{
+    let nthStr1 = str1[n].toLowerCase();
+    let nthStr2 = str2[n].toLowerCase();
+    if (nthStr1 > nthStr2){
+      return 1;
+    }else if (nthStr1 < nthStr2){
+      return -1;
+    }else{
+      if (str1 > str2)
+      {
+        return 1;
+      }else{
+        return -1;
+      }
+    }
+  })
+
+  return strings;
+}
+
+solution(['sun', 'bed', 'car'], 1);
