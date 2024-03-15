b6cc35c65796d5410127f09b781bb6c503735250
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-03-16 02:02:09 +0900

    lv1 solving

---
+++ Lv1/solved/lv1_24.js
@@ -0,0 +1,11 @@
+//프로그래머스 lv1 약수의 개수와 덧셈
+
+function solution(left, right) {
+	//약수의 개수가 홀수개이려면 제곱수여야함.
+	const tempSum = (right - left + 1) * (right + left) / 2;
+	for (let i=left; i<=right; i++){
+		if (Number.isInteger(Math.sqrt(i))){
+			tempSum -= 2 * i;
+		}
+	}
+}
